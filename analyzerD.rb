1ce4ff51d7d0c813239fd8c2711dfb2d0e8aaf8a
GhostNiL <44944461+ghostnil@users.noreply.github.com>
2018-12-19 08:40:20 -0300

    Create analyzerD.rb

---
+++ analyzerD.rb
@@ -0,0 +1,122 @@
+require 'net/http'
+require 'uri'
+require 'optparse'
+
+## inside libs
+load 'shodan_class.rb'
+load 'viewdns.rb'
+load 'censys_search.rb'
+load 'colors.rb'
+
+
+banner_tool = '
+
+    Author: Gh0sTNiL
+    Version: 1.0
+
+################################################################################
+
+
+ █████╗ ███╗   ██╗ █████╗ ██╗  ██╗   ██╗███████╗███████╗██████╗ ██████╗
+██╔══██╗████╗  ██║██╔══██╗██║  ╚██╗ ██╔╝╚══███╔╝██╔════╝██╔══██╗██╔══██╗
+███████║██╔██╗ ██║███████║██║   ╚████╔╝   ███╔╝ █████╗  ██████╔╝██║  ██║
+██╔══██║██║╚██╗██║██╔══██║██║    ╚██╔╝   ███╔╝  ██╔══╝  ██╔══██╗██║  ██║
+██║  ██║██║ ╚████║██║  ██║███████╗██║   ███████╗███████╗██║  ██║██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝   ╚══════╝╚══════╝╚═╝  ╚═╝╚═════╝
+
+
+
+USAGE: ruby analyzerD.rb https://target.com
+    -u, --url TARGET                 Target for crawler
+    -s, --shodan                     [+] Shodan search
+    -d, --viewdns                    [+] ip history from viewDNS
+    -c, --censys                     [+] Censys Search Certificate search, ivp4 and more
+    -a, --all                        [+] Use all methods
+    -h, --help                       Show this entire menu
+
+################################################################################
+'.red
+
+
+def url_parse(url)
+  uri = URI.parse(url)
+  if uri.scheme != "http" && uri.scheme != "https"
+    puts "\n\n[+] You provide a target without a HTTP schema => #{url}\n\n[+] All requests will be made with https://#{url}".red
+    url = "https://" + url
+  end
+  return url
+end
+
+
+## MENU ##
+options = {}
+optparse = OptionParser.new do |opts|
+
+  opts.banner = "USAGE: ruby #{__FILE__} https://target.com"
+
+  opts.on("-u", "--url TARGET", "Target for crawler") do |u|
+    options[:url] = u
+  end
+
+  opts.on("-s", "--shodan", "[+] Shodan search") do |s|
+    options[:shodan] = s
+  end
+
+  opts.on("-d", "--viewdns", "[+] ip history from viewDNS") do |v|
+    options[:viewdns] = v
+  end
+
+  opts.on("-c", "--censys", "[+] Censys Search Certificate search, ivp4 and more") do |c|
+    options[:censys] = c
+  end
+
+  opts.on("-a", "--all", "[+] Use all methods") do |al|
+    options[:all] = al
+  end
+
+  opts.on( '-h', '--help', 'Show this entire menu' ) do
+    puts opts
+    exit
+  end
+end
+optparse.parse!
+
+puts banner_tool
+
+## threat inputs menu
+if options[:url].nil?
+  abort(optparse.help)
+end
+
+url = options[:url]
+parsed_url = url_parse(url)
+url = parsed_url
+
+if options[:shodan]
+  puts "\n\n [+] Verify if your api key is set on shodan_class.rb file\n\n [+] Initialize Shodan Search \n\n".blue
+  shodan_objt = ShodanSearch.new(parsed_url)
+  shodan_objt.shodan_search()
+end
+
+if options[:censys]
+  puts "\n\n [+] Verify if your api key is set on censys_search.rb file\n\n [+] Initialize Censys Search Search \n\n".blue
+  censys_objt = CensysSearch.new(parsed_url)
+  censys_objt.censys_search_ipv4()
+end
+
+if options[:viewdns]
+  puts "\n\n [+] Verify if your api key is set on viewdns.rb file\n\n [+] Initialize viewDNS Search \n\n".blue
+  viewdns_objt = ViewDns.new(parsed_url)
+  viewdns_objt.retrive_info_api()
+end
+
+if options [:all]
+  puts "\n\n[+] USING ALL METHODS [+]\n\n".blue
+  shodan_objt = ShodanSearch.new(parsed_url)
+  shodan_objt.shodan_search()
+  viewdns_objt = ViewDns.new(parsed_url)
+  viewdns_objt.retrive_info_api()
+  censys_objt = CensysSearch.new(parsed_url)
+  censys_objt.censys_search_ipv4()
+  censys_objt.censys_search_websites()
+end
